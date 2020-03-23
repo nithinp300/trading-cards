36dc58e7b716636f89746692825d972c9180a758
Nithin Pingili <nithinp300@gmail.com>
2020-03-22 22:58:25 -0500

    db working

--- data-service/index.js
+++ data-service/index.js
@@ -5,5 +5,24 @@ app.get('/', (req, res) => {
     res.send('This is the Yu-Gi-Oh! Trading Cards RESTful API');
 });
 
-const port = process.env.PORT || 3000
+// db setup
+const db = require("./db");
+const dbName = 'trading-cards';
+const collectionName = 'monsters';
+
+// db init
+db.initialize(dbName, collectionName, function(dbCollection) { // success callback
+    // get all items
+    dbCollection.find().toArray(function(err, result) {
+        if (err) throw err;
+          console.log(result);
+    });
+
+    // db CRUD routes
+
+}, function(err) { // failure callback
+    throw (err);
+});
+
+const port = process.env.PORT || 4000
 app.listen(port, () => console.log(`Listening on port ${port}`));
