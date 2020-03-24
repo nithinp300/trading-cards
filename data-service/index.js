7bd64b212852bfbbf8dd5d210913ec94812e621e
Nithin Pingili <nithinp300@gmail.com>
2020-03-23 20:04:36 -0500

    search using name and desc working

--- data-service/index.js
+++ data-service/index.js
@@ -27,16 +27,24 @@ db.initialize(dbName, collectionName, function(dbCollection) { // success callba
         if(typeof req.query.def !== "undefined"){
             req.query.def = int32(req.query.def);
         }
-        dbCollection.find(req.query).toArray((error, result) => {
-            if (error) throw error;
-            res.json(result);
-        });
+        if(typeof req.query.q !== "undefined"){
+            dbCollection.find({ '$text': {'$search' : req.query.q } }).toArray((error, result) => {
+                if (error) throw error;
+                res.json(result);
+            });
+        }
+        else{
+            dbCollection.find(req.query).toArray((error, result) => {
+                if (error) throw error;
+                res.json(result);
+            });
+        }
     });
 
     app.get("/monsters/:id", (request, response) => {
         let monsterId = request.params.id;
         console.log(monsterId)
-        dbCollection.findOne({ "id": new int32(monsterId) }, (error, result) => {
+        dbCollection.findOne({ id: new int32(monsterId) }, (error, result) => {
             if (error) throw error;
             // return monster
             response.json(result);
