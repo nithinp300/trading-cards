0445fcd9762fbeb1b88349442577bcde0fd75f9e
Nithin Pingili <nithinp300@gmail.com>
2020-03-25 17:19:04 -0500

    all traps that satisfy query parameters route

--- data-service/index.js
+++ data-service/index.js
@@ -17,6 +17,7 @@ db.initialize(dbName, function(dbObject) { // success callback
     // get collections
     const monstersCollection = dbObject.collection('monsters');
     const spellsCollection = dbObject.collection('spells');
+    const trapsCollection = dbObject.collection('traps');
     
     // db GET routes
 
@@ -134,6 +135,62 @@ db.initialize(dbName, function(dbObject) { // success callback
                 }
             }
             spellsCollection.find(filters)
+            .skip(skips).limit(limit).toArray((error, result) => {
+                if (error) throw error;
+                res.json(result);
+            });
+        }
+    });
+
+    app.get("/traps", (req, res) => {
+        // return all traps that satisfy query parameters
+        console.log(req.query);
+        let page = 1;
+        let limit = 5;
+        if(typeof req.query.page !== "undefined"){
+            page = parseInt(req.query.page);
+        }
+        if(typeof req.query.limit !== "undefined"){
+            limit = parseInt(req.query.limit);
+        }
+        let skips = limit * (page - 1);
+        if(typeof req.query.q !== "undefined"){
+            //Search
+            trapsCollection
+            .find({ $text: {$search : req.query.q } })
+            .project({score: {$meta: "textScore"}})
+            .sort({score: {$meta: "textScore"}})
+            .skip(skips).limit(limit).toArray((error, result) => {
+                if (error) throw error;
+                res.json(result);
+            });
+        }
+        else if(typeof req.query.sort !== "undefined"){
+            // Sort
+            let sortBy = new Object();
+            let sortByStr = req.query.sort;
+            let ascDescNum = 1;
+            let firstChar = req.query.sort.substring(0, 1);
+            if(firstChar === "-"){
+                ascDescNum = -1;
+                sortByStr = req.query.sort.substring(1);
+            }
+            sortBy[sortByStr] = ascDescNum;
+            trapsCollection.find().sort(sortBy)
+            .skip(skips).limit(limit).toArray((error, result) => {
+                if (error) throw error;
+                res.json(result);
+            });
+        }
+        else{
+            // Filter
+            let filters = new Object();
+            for(let i in req.query){
+                if(i !== "page" && i !== "limit"){
+                    filters[i] = req.query[i];
+                }
+            }
+            trapsCollection.find(filters)
             .skip(skips).limit(limit).toArray((error, result) => {
                 if (error) throw error;
                 res.json(result);
