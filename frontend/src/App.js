9904bf2a998b33bc89a644be69a7cfc2416f63b3
Nithin Pingili <nithinp300@gmail.com>
2020-04-03 18:33:31 -0500

    created navbar and set up routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Nav, Navbar, Form, FormControl, Button} from 'react-bootstrap';
+import {
+    BrowserRouter,
+    Switch,
+    Route
+  } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+  return(
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="/">Yu-Gi-Oh! Deck</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/monsters">Monsters</Nav.Link>
+          <Nav.Link href="/spells">Spells</Nav.Link>
+          <Nav.Link href="/traps">Traps</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-info">Search</Button>
+        </Form>
+      </Navbar>
+      <BrowserRouter>
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+          <Route path="/Traps">
+              <Traps />
+          </Route>
+          <Route path="/Spells">
+              <Spells />
+          </Route>
+          <Route path="/Monsters">
+              <Monsters />
+          </Route>
+          <Route path="/">
+              <Home />
+          </Route>
+          </Switch>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App;
+function Home() {
+    return <h2>Home</h2>;
+  }
+  
+  function Monsters() {
+    return <h2>Monsters</h2>;
+  }
+  
+  function Spells() {
+    return <h2>Spells</h2>;
+  }
+
+  function Traps() {
+    return <h2>Traps</h2>;
+  }
