1574953c81eb64a9fae8909762e6cd3bac5c86c5
Nithin Pingili <nithinp300@gmail.com>
2020-04-03 20:50:15 -0500

    working on fetching api

--- frontend/src/Monsters.js
+++ frontend/src/Monsters.js
@@ -2,33 +2,39 @@ import React, {Component} from 'react';
 import MonsterCard from './MonsterCard'
 import PaginationBar from './PaginationBar'
 
-class Monsters extends Component{
+class Monsters extends Component {
   constructor(){
     super();
-    this.state = [];
+    this.state = {
+      monsters: []
+    };
   }
 
   componentDidMount(){
     fetch("http://localhost:5000/monsters")
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => {
+        this.setState({
+          monsters: data
+        })
+      })
   }
 
   render(){
+    console.log(this.state.monsters.length);
     return(
       <div>
+        <h2>{this.state.monsters.name}</h2>
         <div class="row">
-          <div class="col-sm-3"><MonsterCard monster={{name: "Nithin", desc: "This is a description"}}></MonsterCard></div>
-          <div class="col-sm-3"><MonsterCard monster={{name: "Nithin", desc: "This is a description"}}></MonsterCard></div>
-          <div class="col-sm-3"><MonsterCard monster={{name: "Nithin", desc: "This is a description"}}></MonsterCard></div>
-          <div class="col-sm-3"><MonsterCard monster={{name: "Nithin", desc: "This is a description"}}></MonsterCard></div>
+          <div class="col-sm-4"><MonsterCard monster={{name: "Nithin", desc: "This is a description"}}></MonsterCard></div>
+          <div class="col-sm-4"><MonsterCard monster={{name: "Nithin", desc: "This is a description"}}></MonsterCard></div>
+          <div class="col-sm-4"><MonsterCard monster={{name: "Nithin", desc: "This is a description"}}></MonsterCard></div>
         </div>
         <br></br>
         <div class="row">
-            <div class="col-sm-3"><MonsterCard monster={{name: "Nithin", desc: "This is a description"}}></MonsterCard></div>
-            <div class="col-sm-3"><MonsterCard monster={{name: "Nithin", desc: "This is a description"}}></MonsterCard></div>
-            <div class="col-sm-3"><MonsterCard monster={{name: "Nithin", desc: "This is a description"}}></MonsterCard></div>
-            <div class="col-sm-3"><MonsterCard monster={{name: "Nithin", desc: "This is a description"}}></MonsterCard></div>
+          <div class="col-sm-4"><MonsterCard monster={{name: "Nithin", desc: "This is a description"}}></MonsterCard></div>
+          <div class="col-sm-4"><MonsterCard monster={{name: "Nithin", desc: "This is a description"}}></MonsterCard></div>
+          <div class="col-sm-4"><MonsterCard monster={{name: "Nithin", desc: "This is a description"}}></MonsterCard></div>
         </div>
         <br></br>
         <div>
