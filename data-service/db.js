b6ab033ccc36f52e8d687af6c91eb030a5e420f9
Nithin Pingili <nithinp300@gmail.com>
2020-04-06 20:56:48 -0500

    created env file

--- data-service/db.js
+++ data-service/db.js
@@ -1,8 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const fs = require('fs');
-
-const credentials = fs.readFileSync('../db_creds.txt','utf8');
-const dbConnectionUrl = 'mongodb+srv://'+credentials+'@cluster0-p7o7f.mongodb.net/test?retryWrites=true&w=majority';
+require('dotenv').config()
 
 function initialize(
     dbName,
@@ -10,7 +7,7 @@ function initialize(
     successCallback,
     failureCallback
 ) {
-    MongoClient.connect(dbConnectionUrl, function(err, dbInstance) {
+    MongoClient.connect(process.env.DATABASE_URL, function(err, dbInstance) {
         if (err) {
             console.log(`[MongoDB connection] ERROR: ${err}`);
             failureCallback(err); // this should be "caught" by the calling function
