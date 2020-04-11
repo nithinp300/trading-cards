0edbcda71d0ff93220ff9d5a451725cd37035fa3
Nithin Pingili <nithinp300@gmail.com>
2020-04-11 16:44:33 -0500

    sorting and attribute filter working

--- frontend/src/components/Monsters.js
+++ frontend/src/components/Monsters.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import PaginationBar from './PaginationBar'
-import {Container, Row, Col, Dropdown, DropdownButton, Spinner} from 'react-bootstrap'
+import {Container, Row, Col, Spinner} from 'react-bootstrap'
 import TradingCard from './TradingCard'
 import MonstersData from './monsters_data'
 import MonsterCard from './MonsterCard'
@@ -16,20 +16,25 @@ class Monsters extends Component {
       per_page: MonstersData.per_page,
       current_page: MonstersData.page,
       last_page: MonstersData.total_pages,
+      sortBy: "name",
       attribute: ""
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handlePaginationClick = this.handlePaginationClick.bind(this);
   }
 
   componentDidMount(){
-    this.makeHttpRequestWithPage(1, "")
+    this.makeHttpRequestWithPage(1, {})
   }
 
-  makeHttpRequestWithPage = async (pageNumber, attribute) => {
-    if(!attribute){
-      attribute = this.state.attribute
+  makeHttpRequestWithPage = async (pageNumber, params) => {
+    if(typeof params.attribute === "undefined"){
+      params.attribute = this.state.attribute
     }
-    const response = await fetch(`https://yugioh-data-service.herokuapp.com/monsters?limit=6&page=${pageNumber}&attribute=${attribute}`, {
+    if(typeof params.sortBy === "undefined"){
+      params["sortBy"] = this.state.sortBy
+    }
+    const response = await fetch(`https://yugioh-data-service.herokuapp.com/monsters?page=${pageNumber}&sort=${params.sortBy}&attribute=${params.attribute}`, {
       method: 'GET',
       headers: {
         'Accept': 'application/json',
@@ -50,7 +55,6 @@ class Monsters extends Component {
   }
 
   makeRequest = async pageNumber => {
-    document.querySelectorAll('input[type=checkbox]').forEach( el => el.checked = false );
     fetch(`https://yugioh-data-service.herokuapp.com/monsters?limit=6&page=${pageNumber}`)
     .then(res => res.json())
     .then(data => {
@@ -70,9 +74,14 @@ class Monsters extends Component {
     this.makeHttpRequestWithPage(1)
   }
 
-  handleChange(event){
-    this.setState({attribute: event.target.value, isLoading: true})
-    this.makeHttpRequestWithPage(1, event.target.value)
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value, isLoading: true})
+    this.makeHttpRequestWithPage(1, {[event.target.name]: event.target.value})
+  }
+
+  handlePaginationClick(page) {
+    this.setState({isLoading: true})
+    this.makeHttpRequestWithPage(page, {})
   }
 
   render(){
@@ -88,22 +97,23 @@ class Monsters extends Component {
       <div>
         <br></br>
         <div style={{float:"right"}}>
-          <DropdownButton id="dropdown-basic-button" title="Sort by">
-            <Dropdown.Item href="#/action-1">Name: A to Z</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Name: Z to A</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">ATK: Low to High</Dropdown.Item>
-            <Dropdown.Item href="#/action-4">ATK: High to Low</Dropdown.Item>
-            <Dropdown.Item href="#/action-4">DEF: Low to High</Dropdown.Item>
-            <Dropdown.Item href="#/action-5">DEF: High to Low</Dropdown.Item>
-            <Dropdown.Item href="#/action-5">Level: Low to High</Dropdown.Item>
-            <Dropdown.Item href="#/action-5">Level: High to Low</Dropdown.Item>
-          </DropdownButton>
+          <label>Sort by &nbsp;</label>
+          <select value={this.state.sortBy} onChange={this.handleChange} name="sortBy">
+            <option value="name">Name: A to Z</option>
+            <option value="-name">Name: Z to A</option>
+            <option value="atk">ATK: Low to High</option>
+            <option value="-atk">ATK: High to Low</option>
+            <option value="def">DEF: Low to High</option>
+            <option value="-def">DEF: High to Low</option>
+            <option value="level">Level: Low to High</option>
+            <option value="-level">Level: High to Low</option>
+          </select>
         </div>
         <Container fluid="md">
           <Row >
-            <label>Attribute &nbsp;</label>
+            <label>Attribute: &nbsp;</label>
             <select value={this.state.attribute} onChange={this.handleChange} name="attribute">
-              <option value="">Choose...</option>
+              <option value="">Choose</option>
               <option value="DARK">Dark</option>
               <option value="DIVINE">Divine</option>
               <option value="EARTH">Earth</option>
@@ -112,31 +122,30 @@ class Monsters extends Component {
               <option value="WATER">Water</option>
               <option value="WIND">Wind</option>
             </select>
-            <label>&nbsp; Monster Type &nbsp;</label>
+            <label>&nbsp; Monster Type: &nbsp;</label>
             <select value={this.state.type} name="type">
             </select>
-            <label>&nbsp; Archetype &nbsp;</label>
+            <label>&nbsp; Archetype: &nbsp;</label>
             <select value={this.state.type} name="type">
             </select>
           </Row>
           <Row>
             {isLoaded}
           </Row>
-          <br></br>
-          <Row className="justify-content-md-center">
-            <Col md="3">
+          <Row>
+            <div style={{margin:"auto"}}>
               <Pagination>
                 <Pagination.First disabled={this.state.current_page === 1} 
-                onClick={() => this.makeHttpRequestWithPage(1)}/>
-                <Pagination.Prev disabled={this.state.current_page === 1} 
-                onClick={() => this.makeHttpRequestWithPage(this.state.current_page-1)}/>
+                onClick={() => this.handlePaginationClick(1)}/>
+                <Pagination.Prev disabled={this.state.current_page === 1}
+                onClick={() => this.handlePaginationClick(this.state.current_page-1)}/>
                 <Pagination.Item>{this.state.current_page}</Pagination.Item>
                 <Pagination.Next disabled={this.state.current_page === this.state.last_page} 
-                onClick={() => this.makeHttpRequestWithPage(this.state.current_page+1)}/>
+                onClick={() => this.handlePaginationClick(this.state.current_page+1)}/>
                 <Pagination.Last disabled={this.state.current_page === this.state.last_page} 
-                onClick={() => this.makeHttpRequestWithPage(this.state.last_page)}/>
+                onClick={() => this.handlePaginationClick(this.state.last_page)}/>
               </Pagination>
-            </Col>
+            </div>
           </Row>
         </Container>
       </div>
