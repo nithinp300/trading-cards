f90c861d93287750a8b541afc18242decec73c5c
Nithin Pingili <nithinp300@gmail.com>
2020-04-10 23:28:18 -0500

    added both filter and sort and checked query params for empty strings

--- data-service/routes/monsters.js
+++ data-service/routes/monsters.js
@@ -11,72 +11,63 @@ db.initialize(dbName, collectionName, function(monstersCollection) { // success 
     // return all monsters that satisfy query parameters
     console.log(req.query);
     let page = 1;
-    let limit = 5;
-    if(typeof req.query.page !== "undefined") {
+    let limit = 6;
+    if(typeof req.query.page !== "undefined" && req.query.page !== "") {
       page = parseInt(req.query.page);
     }
-    if(typeof req.query.limit !== "undefined") {
+    if(typeof req.query.limit !== "undefined" && req.query.limit !== "") {
       limit = parseInt(req.query.limit);
     }
-    if(typeof req.query.level !== "undefined") {
-      req.query.level = int32(req.query.level);
+    let params = new Object();
+    // only get search, sort and filter params
+    for(let i in req.query){
+      if(i !== "page" && i !== "limit" && req.query[i] !== "") {
+        params[i] = req.query[i];
+      }
     }
-    if(typeof req.query.atk !== "undefined") {
-      req.query.atk = int32(req.query.atk);
+    if(typeof params.level !== "undefined") {
+      params.level = int32(params.level);
     }
-    if(typeof req.query.def !== "undefined") {
-      req.query.def = int32(req.query.def);
+    if(typeof params.atk !== "undefined") {
+      params.atk = int32(params.atk);
+    }
+    if(typeof params.def !== "undefined") {
+      params.def = int32(params.def);
     }
     let skips = limit * (page - 1);
-    if(typeof req.query.q !== "undefined") {
+    if(typeof params.q !== "undefined") {
       //Search
       monstersCollection
       .find({ $text: {$search : req.query.q } })
       .project({score: {$meta: "textScore"}})
       .sort({score: {$meta: "textScore"}})
-      .skip(skips).limit(limit).toArray((error, result) => {
+      .toArray((error, result) => {
         if (error) throw error;
         res.json(result);
       });
     }
-    else if(typeof req.query.sort !== "undefined") {
-      // Sort
-      let sortBy = new Object();
-      let sortByStr = req.query.sort;
-      let ascDescNum = 1;
-      let firstChar = req.query.sort.substring(0, 1);
-      if(firstChar === "-"){
-        ascDescNum = -1;
-        sortByStr = req.query.sort.substring(1);
-      }
-      sortBy[sortByStr] = ascDescNum;
-      monstersCollection.find().sort(sortBy)
-      .skip(skips).limit(limit).toArray((error, data) => {
-        if (error) throw error;
-        monstersCollection.countDocuments({}, (error, count) => {
-          if (error) throw error;
-          let result = new Object();
-          result.page = page;
-          result.per_page = limit;
-          result.total = count;
-          result.total_pages = Math.ceil(count / limit);
-          result.data = data;
-          res.json(result);
-        });
-      });
-    }
     else {
-      // Filter
-      let filters = new Object();
-      for(let i in req.query){
-        if(i !== "page" && i !== "limit" && req.query[i] !== ""){
-            filters[i] = req.query[i];
-        }
+      let sortBy = new Object();
+      if(typeof params.sort === "undefined"){
+        // by default we sort by name alphabetically
+        sortBy["name"] = 1;
       }
-      monstersCollection.find(filters)
+      else{
+        // use sort parameter and value given
+        let sortByStr = params.sort;
+        let ascDescNum = 1;
+        let firstChar = params.sort.substring(0, 1);
+        if(firstChar === "-"){
+          ascDescNum = -1;
+          sortByStr = params.sort.substring(1);
+        }
+        sortBy[sortByStr] = ascDescNum;
+      }
+      delete params.sort;
+      monstersCollection.find(params).sort(sortBy)
       .skip(skips).limit(limit).toArray((error, data) => {
           if (error) throw error;
-          monstersCollection.countDocuments(filters, (error, count) => {
+          monstersCollection.countDocuments(params, (error, count) => {
             if (error) throw error;
             let result = new Object();
             result.page = page;
@@ -98,6 +89,7 @@ db.initialize(dbName, collectionName, function(monstersCollection) { // success 
       res.json(result);
     });
   });
+
 }, function(error) { // failureCallback
     throw (error);
 });
--- data-service/routes/traps.js
+++ data-service/routes/traps.js
@@ -7,88 +7,79 @@ const dbName = 'trading-cards';
 const collectionName = 'traps';
 
 db.initialize(dbName, collectionName, function(trapsCollection) { // success callback
-    traps.get("/", (req, res) => {
-        // return all traps that satisfy query parameters
-        console.log(req.query);
-        let page = 1;
-        let limit = 5;
-        if(typeof req.query.page !== "undefined"){
-            page = parseInt(req.query.page);
+  traps.get("/", (req, res) => {
+    // return all traps that satisfy query parameters
+    console.log(req.query);
+    let page = 1;
+    let limit = 6;
+    if(typeof req.query.page !== "undefined" && req.query.page !== "") {
+      page = parseInt(req.query.page);
+    }
+    if(typeof req.query.limit !== "undefined" && req.query.limit !== "") {
+      limit = parseInt(req.query.limit);
+    }
+    let params = new Object();
+    // only get search, sort and filter params
+    for(let i in req.query){
+      if(i !== "page" && i !== "limit" && req.query[i] !== "") {
+        params[i] = req.query[i];
+      }
+    }
+    let skips = limit * (page - 1);
+    if(typeof params.q !== "undefined") {
+      //Search
+      trapsCollection
+      .find({ $text: {$search : req.query.q } })
+      .project({score: {$meta: "textScore"}})
+      .sort({score: {$meta: "textScore"}})
+      .toArray((error, result) => {
+        if (error) throw error;
+        res.json(result);
+      });
+    }
+    else {
+      let sortBy = new Object();
+      if(typeof params.sort === "undefined"){
+        // by default we sort by name alphabetically
+        sortBy["name"] = 1;
+      }
+      else{
+        // use sort parameter and value given
+        let sortByStr = params.sort;
+        let ascDescNum = 1;
+        let firstChar = params.sort.substring(0, 1);
+        if(firstChar === "-"){
+          ascDescNum = -1;
+          sortByStr = params.sort.substring(1);
         }
-        if(typeof req.query.limit !== "undefined"){
-            limit = parseInt(req.query.limit);
-        }
-        let skips = limit * (page - 1);
-        if(typeof req.query.q !== "undefined"){
-            //Search
-            trapsCollection
-            .find({ $text: {$search : req.query.q } })
-            .project({score: {$meta: "textScore"}})
-            .sort({score: {$meta: "textScore"}})
-            .skip(skips).limit(limit).toArray((error, result) => {
-                if (error) throw error;
-                res.json(result);
-            });
-        }
-        else if(typeof req.query.sort !== "undefined"){
-            // Sort
-            let sortBy = new Object();
-            let sortByStr = req.query.sort;
-            let ascDescNum = 1;
-            let firstChar = req.query.sort.substring(0, 1);
-            if(firstChar === "-"){
-                ascDescNum = -1;
-                sortByStr = req.query.sort.substring(1);
-            }
-            sortBy[sortByStr] = ascDescNum;
-            trapsCollection.find().sort(sortBy)
-            .skip(skips).limit(limit).toArray((error, data) => {
-                if (error) throw error;
-                trapsCollection.countDocuments({}, (error, count) => {
-                  if (error) throw error;
-                  let result = new Object();
-                  result.page = page;
-                  result.per_page = limit;
-                  result.total = count;
-                  result.total_pages = Math.ceil(count / limit);
-                  result.data = data;
-                  res.json(result);
-                });
-            });
-        }
-        else{
-            // Filter
-            let filters = new Object();
-            for(let i in req.query){
-                if(i !== "page" && i !== "limit" && req.query[i] !== ""){
-                    filters[i] = req.query[i];
-                }
-            }
-            trapsCollection.find(filters)
-            .skip(skips).limit(limit).toArray((error, data) => {
-                if (error) throw error;
-                trapsCollection.countDocuments(filters, (error, count) => {
-                  if (error) throw error;
-                  let result = new Object();
-                  result.page = page;
-                  result.per_page = limit;
-                  result.total = count;
-                  result.total_pages = Math.ceil(count / limit);
-                  result.data = data;
-                  res.json(result);
-                });
-            });
-        }
-    });
-
-    traps.get("/:trapId", (req, res) => {
-        console.log(req.params.trapId);
-        trapsCollection.findOne({ id: new int32(req.params.trapId) }, (error, result) => {
+        sortBy[sortByStr] = ascDescNum;
+      }
+      delete params.sort;
+      trapsCollection.find(params).sort(sortBy)
+      .skip(skips).limit(limit).toArray((error, data) => {
+          if (error) throw error;
+          trapsCollection.countDocuments(params, (error, count) => {
             if (error) throw error;
-            // return trap
+            let result = new Object();
+            result.page = page;
+            result.per_page = limit;
+            result.total = count;
+            result.total_pages = Math.ceil(count / limit);
+            result.data = data;
             res.json(result);
-        });
+          });
+      });
+    }
+  });
+
+  traps.get("/:trapId", (req, res) => {
+    console.log(req.params.trapId);
+    trapsCollection.findOne({ id: new int32(req.params.trapId) }, (error, result) => {
+      if (error) throw error;
+      // return trap
+      res.json(result);
     });
+  });
 
 }, function(err) { // failureCallback
     throw (err);
