b46ca0df1f9be8c77d66a5c753f2598be52a0a89
Nithin Pingili <nithinp300@gmail.com>
2020-04-26 23:07:45 -0500

    react hooks

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,8 +1,15 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect} from "react";
 import { useAuth0 } from "../react-auth0-spa";
+import { Tab } from "react-bootstrap";
 
-const Profile = () => {
+function Profile(){
   const { loading, user } = useAuth0();
+  const [count, setCount] = useState(0);
+  const [file, setFile] = useState('not uploaded');
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
 
   if (loading || !user) {
     return <div>Loading...</div>;
@@ -11,12 +18,46 @@ const Profile = () => {
   return (
     <Fragment>
       <img src={user.picture} alt="Profile" />
-
       <h2>{user.name}</h2>
       <p>{user.email}</p>
       <code>{JSON.stringify(user, null, 2)}</code>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+      <form onSubmit={() => setFile('uploaded')}>
+        <label for="myfile">Select a file:</label>
+        <input type="file" id="myfile" name="myfile"></input>
+        <input type="submit" value="submit"></input>
+      </form>
+      <p>File was {file}</p>
     </Fragment>
   );
 };
+
+function fileData(){
+     
+  if (this.state.selectedFile) { 
+      
+    return ( 
+      <div> 
+        <h2>File Details:</h2> 
+        <p>File Name: {this.state.selectedFile.name}</p> 
+        <p>File Type: {this.state.selectedFile.type}</p> 
+        <p> 
+          Last Modified:{" "} 
+          {this.state.selectedFile.lastModifiedDate.toDateString()} 
+        </p> 
+      </div> 
+    ); 
+  } else { 
+    return ( 
+      <div> 
+        <br /> 
+        <h4>Choose before Pressing the Upload button</h4> 
+      </div> 
+    ); 
+  } 
+}; 
 
 export default Profile;
