809d77207f076272d04748f5fdc2ab91cd197686
Nithin Pingili <nithinp300@gmail.com>
2020-05-09 00:21:42 -0500

    consumed user api

--- frontend/src/components/Deck.js
+++ frontend/src/components/Deck.js
@@ -1,8 +1,43 @@
 import React, { Component } from "react";
+import axios from 'axios';
+import {Container, Row, Col, Spinner} from 'react-bootstrap'
+import TradingCard from './TradingCard'
 
 class Deck extends Component {
+  constructor(){
+    super();
+    this.state = {
+      cards : []
+    }
+  }
+  
+  componentDidMount(){
+    axios.get(`http://localhost:5000/api/cards`)
+      .then(res => {
+        const cards = res.data;
+        this.setState({ cards });
+      })
+  }
+
   render(){
-    return("Hi")
+    let url = "/monsters"
+    let monsterCards = this.state.cards.map(monster => {
+      return(
+        <Col md="4">
+          <TradingCard monster={monster} url={url}></TradingCard>
+        </Col>
+      )
+    })
+    return(
+      <Container fluid="md">
+        <Row>
+          <p>&nbsp;</p>
+        </Row>
+        <Row>
+          {monsterCards}
+        </Row>
+      </Container>
+    );
   }
 }
 
