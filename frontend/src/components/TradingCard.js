0580aef3a8a8dfacbb071ebd570995d1fa95370b
Nithin Pingili <nithinp300@gmail.com>
2020-05-10 00:02:23 -0500

    consumed remove card

--- frontend/src/components/TradingCard.js
+++ frontend/src/components/TradingCard.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {Card, Button} from 'react-bootstrap'
+import {Button} from 'react-bootstrap'
 import {Link, useRouteMatch} from 'react-router-dom';
 import { useAuth0 } from "../react-auth0-spa";
 import axios from 'axios';
@@ -8,11 +8,21 @@ function TradingCard(props) {
   function handleAddClick(){
     let cardData = props.monster
     console.log(cardData)
-    // make a post method with card data
+    // make a post request with card data
     axios.post(`http://localhost:5000/api/cards`, cardData)
       .then(res => {
         console.log(res);
+        console.log(res.status);
+        alert(`${cardData.name} was added to your Deck!`);
+      })
+  }
+
+  function handleRemoveClick(){
+    axios.delete(`http://localhost:5000/api/cards/${props.monster.id}`)
+      .then(res => {
+        console.log(res);
         console.log(res.data);
+        alert(`${props.monster.name} was removed from your Deck`);
       })
   }
   const { isAuthenticated } = useAuth0();
@@ -21,7 +31,6 @@ function TradingCard(props) {
   // useEffect(() => {
 
   // });
-
   if (!props.monster.card_images) return null;
   return(
     <div style={{margin:"2rem"}}>
@@ -29,37 +38,9 @@ function TradingCard(props) {
         <img src={props.monster.card_images[0].image_url} alt={props.monster.name} height="400" width="300"></img>
       </Link>
       <button onClick={handleAddClick}>Add</button>
-      {/* {isAuthenticated && <button onClick={handleAddClick(props.monster)}>Add</button>} */}
-      {/* <Card style={{margin:"1.5rem"}}>
-        <Link to={`${props.url}/${props.monster.id}`} style={{textDecoration:"none"}}>
-          <Card.Img variant="top" src={props.monster.card_images[0].image_url} />
-          <Card.Body>
-            <Card.Title>{props.monster.name}</Card.Title>
-            <Card.Text>
-              Classification: {props.monster.type}<br></br>
-              Attribute: {props.monster.attribute}<br></br>
-              Type: {props.monster.race}<br></br>
-              Level: {props.monster.level}<br></br>
-              ATK: {props.monster.atk}<br></br>
-              DEF: {props.monster.def}<br></br>
-            </Card.Text>
-          </Card.Body>
-        </Link>
-        {isAuthenticated && <button >Add</button>}
-      </Card> */}
+      <button onClick={handleRemoveClick}>Remove</button>
     </div>
   )
 }
 
 export default TradingCard
-
-// function handleAddClick(props){
-//   let cardData = props.monster
-//   console.log(cardData)
-//   // make a post method with card data
-//   axios.post(`http://localhost:5000/api/cards`, { cardData })
-//     .then(res => {
-//       console.log(res);
-//       console.log(res.data);
-//     })
-// }
