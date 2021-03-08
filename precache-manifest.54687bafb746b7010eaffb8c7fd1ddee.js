84fbabb401239c99c4153b541f14e68d3b579f69
Nithin Pingili <nithinp300@gmail.com>
2021-03-07 19:26:26 -0600

    Updates

---
+++ precache-manifest.54687bafb746b7010eaffb8c7fd1ddee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc7ac2e1396c97c1f99507e6ed7d5062",
+    "url": "/trading-cards/index.html"
+  },
+  {
+    "revision": "ecf889f964ab81481421",
+    "url": "/trading-cards/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "762adff72e69b5773e88",
+    "url": "/trading-cards/static/css/main.ef9ab79f.chunk.css"
+  },
+  {
+    "revision": "ecf889f964ab81481421",
+    "url": "/trading-cards/static/js/2.70060d08.chunk.js"
+  },
+  {
+    "revision": "0071f32a1be6d8e479e4732d81f47ca6",
+    "url": "/trading-cards/static/js/2.70060d08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "762adff72e69b5773e88",
+    "url": "/trading-cards/static/js/main.91528b6b.chunk.js"
+  },
+  {
+    "revision": "017ca76ca7abec07e5b0",
+    "url": "/trading-cards/static/js/runtime-main.06baa942.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/trading-cards/precache-manifest.54687bafb746b7010eaffb8c7fd1ddee.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trading-cards/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
